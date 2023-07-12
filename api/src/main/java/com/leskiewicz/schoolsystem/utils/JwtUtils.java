05b87bb840cee22e660f9a4cf0e6d59cc5c80367
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-07-12 21:10:49 +0200

    Working on jwt

---
+++ api/src/main/java/com/leskiewicz/schoolsystem/utils/JwtUtils.java
@@ -0,0 +1,18 @@
+package com.leskiewicz.schoolsystem.utils;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+public interface JwtUtils {
+
+    String extractUsername(String token);
+    String generateToken(UserDetails userDetails);
+    String generateToken(Map<String, Object> extraClaims, UserDetails userDetails);
+    boolean isTokenValid(String token, UserDetails userDetails);
+    <T> T extractClaim(String token, Function<Claims, T> claimsResolver);
+}
