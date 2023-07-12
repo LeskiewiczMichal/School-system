8ac38acddc0489a55857dffe19950edf0e58b566
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-07-13 01:51:03 +0200

    Bad credentials exception

--- api/src/main/java/com/leskiewicz/schoolsystem/error/DefaultExceptionHandler.java
+++ api/src/main/java/com/leskiewicz/schoolsystem/error/DefaultExceptionHandler.java
@@ -8,6 +8,7 @@ import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -20,38 +21,6 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class DefaultExceptionHandler {
-
-//    @ExceptionHandler(value = { MethodArgumentNotValidException.class })
-
-//    @Override
-//    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-//        BindingResult result = ex.getBindingResult();
-//        List<FieldError> fieldErrors = result.getFieldErrors();
-//        Map<String, String> errors = new HashMap<>();
-//        for (FieldError fieldError : fieldErrors) {
-//            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
-//        }
-//        return ResponseEntity.badRequest().body(errors);
-//    }
-
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ApiError> handleMessageNotReadableException(HttpMessageNotReadableException ex, HttpServletRequest request) {
-        String errorMessage = "Invalid request body";
-        if (ex.getCause() instanceof InvalidFormatException) {
-            InvalidFormatException invalidFormatException = (InvalidFormatException) ex.getCause();
-            String fieldName = invalidFormatException.getPath().stream()
-                    .map(JsonMappingException.Reference::getFieldName)
-                    .collect(Collectors.joining("."));
-            errorMessage = "Invalid value for field '" + fieldName + "'";
-        }
-        ApiError apiError = new ApiError(
-                request.getRequestURI(),
-                errorMessage,
-                HttpStatus.BAD_REQUEST.value(),
-                LocalDateTime.now()
-        );
-        return ResponseEntity.badRequest().body(apiError);
-    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiError> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpServletRequest request) {
@@ -79,6 +48,30 @@ public class DefaultExceptionHandler {
         );
 
         return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiError> handleBadCredentialsException(BadCredentialsException ex, HttpServletRequest request) {
+        ApiError apiError = new ApiError(
+                request.getRequestURI(),
+                "Incorrect email or password",
+                HttpStatus.UNAUTHORIZED.value(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(apiError, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleException(Exception ex, HttpServletRequest request) {
+        ApiError apiError = new ApiError(
+                request.getRequestURI(),
+                ex.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
