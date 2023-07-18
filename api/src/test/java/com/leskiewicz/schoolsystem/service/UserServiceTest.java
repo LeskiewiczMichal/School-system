668bde59602522b9738808d89c2bd38ef7981bae
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-07-18 14:07:49 +0200

    Added some user service tests

--- api/src/test/java/com/leskiewicz/schoolsystem/service/UserServiceTest.java
+++ api/src/test/java/com/leskiewicz/schoolsystem/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.leskiewicz.schoolsystem.service;
 import com.leskiewicz.schoolsystem.degree.Degree;
 import com.leskiewicz.schoolsystem.degree.DegreeService;
 import com.leskiewicz.schoolsystem.degree.DegreeTitle;
+import com.leskiewicz.schoolsystem.error.UserAlreadyExistsException;
 import com.leskiewicz.schoolsystem.faculty.Faculty;
 import com.leskiewicz.schoolsystem.faculty.FacultyService;
 import com.leskiewicz.schoolsystem.security.Role;
@@ -10,6 +11,7 @@ import com.leskiewicz.schoolsystem.user.User;
 import com.leskiewicz.schoolsystem.user.UserRepository;
 import com.leskiewicz.schoolsystem.user.UserService;
 import com.leskiewicz.schoolsystem.user.UserServiceImpl;
+import com.leskiewicz.schoolsystem.user.dto.UserDto;
 import com.leskiewicz.schoolsystem.user.utils.UserMapper;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Assertions;
@@ -18,13 +20,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -108,6 +115,53 @@ public class UserServiceTest {
 
         Assertions.assertThrows(EntityNotFoundException.class, () ->
                 userService.getByEmail("email@example/com"));
+    }
+    //endregion
+
+    //region GetUsers tests
+    @Test
+    public void getUsersReturnsPagedUsers() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by(Sort.Direction.fromString("asc"), "id"));
+        Page<User> mockPage = Mockito.mock(Page.class);
+
+        given(userRepository.findAll(pageable)).willReturn(mockPage);
+
+        Page<User> users = userService.getUsers(pageable);
+        Assertions.assertEquals(users, mockPage);
+    }
+    //endregion
+
+    //region ToUserDtos tests
+    @Test
+    public void toUserDtosReturnsListOfUserDtos() {
+        Page<User> userPage = new PageImpl<>(Arrays.asList(user, user));
+        UserDto userDto = new UserDto();
+        given(userMapper.convertToDto(any(User.class))).willReturn(userDto);
+
+        List<UserDto> result = userService.toUserDtos(userPage);
+
+        Assertions.assertEquals(2, result.size());
+        Assertions.assertEquals(userDto, result.get(0));
+        Assertions.assertEquals(userDto, result.get(1));
+    }
+    //endregion
+
+    //region AddUser tests
+    @Test
+    public void addUserSavesUser() {
+        given(userRepository.findByEmail(any(String.class))).willReturn(Optional.empty());
+
+        userService.addUser(user);
+
+        verify(userRepository).save(user);
+    }
+
+    @Test
+    public void throwsUserAlreadyExistsExceptionWhenUserWithGivenEmailAlreadyExists() {
+        given(userRepository.findByEmail(any(String.class))).willReturn(Optional.of(user));
+
+        Assertions.assertThrows(UserAlreadyExistsException.class, () ->
+                userService.addUser(user));
     }
     //endregion
 }
