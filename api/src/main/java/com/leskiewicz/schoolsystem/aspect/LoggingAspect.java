3293da1b7d9ddad8b7537c0e08958f79a95dabec
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-07-20 00:32:27 +0200

    Aspect logging for service and controller

--- api/src/main/java/com/leskiewicz/schoolsystem/aspect/LoggingAspect.java
+++ api/src/main/java/com/leskiewicz/schoolsystem/aspect/LoggingAspect.java
@@ -23,28 +23,55 @@ public class LoggingAspect {
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
   @Pointcut("within(com.leskiewicz.schoolsystem..*) && (@within(org.springframework.stereotype.Controller) || @within(org.springframework.web.bind.annotation.RestController))")
-  public void controllerMethodsPointcut() {
+    public void controllerMethodsPointcut() {
   }
 
+  @Pointcut("within(com.leskiewicz.schoolsystem..*) && @within(org.springframework.stereotype.Service)")
+  public void nonControllerMethodsPointcut() {
+  }
+
+  //region Controller logs
   @Before("controllerMethodsPointcut()")
-  public void logBefore(JoinPoint joinPoint) {
-    Object[] args = joinPoint.getArgs();
-    Object[] maskedArgs = new Object[args.length];
-    for (int i = 0; i < args.length; i++) {
-      maskedArgs[i] = LoggingUtils.maskSensitiveInformation(args[i]);
-    }
+  public void logInfoBefore(JoinPoint joinPoint) {
+    // Mask each argument
+    Object[] maskedArgs = maskArguments(joinPoint.getArgs());
     logger.info("Entering method: {} with arguments: {}", joinPoint.getSignature().toShortString(),
         maskedArgs);
   }
 
   @AfterReturning(pointcut = "controllerMethodsPointcut()", returning = "result")
-  public void logAfterReturning(JoinPoint joinPoint, Object result) {
+  public void logInfoAfterReturning(JoinPoint joinPoint, Object result) {
+    // Mask result
     Object maskedResult = LoggingUtils.maskSensitiveInformation(result);
     logger.info("Exiting method: {} with result: {}", joinPoint.getSignature().toShortString(),
         maskedResult);
   }
+  //endregion
 
+  //region Service logs
+  @Before("nonControllerMethodsPointcut()")
+  public void logDebugBefore(JoinPoint joinPoint) {
+    // Mask each argument
+    Object[] maskedArgs = maskArguments(joinPoint.getArgs());
+    logger.debug("Entering method: {} with arguments: {}", joinPoint.getSignature().toShortString(),
+        maskedArgs);
+  }
 
+  @AfterReturning(pointcut = "nonControllerMethodsPointcut()", returning = "result")
+  public void logDebugAfterReturning(JoinPoint joinPoint, Object result) {
+    // Mask result
+    Object maskedResult = LoggingUtils.maskSensitiveInformation(result);
+    logger.debug("Exiting method: {} with result: {}", joinPoint.getSignature().toShortString(),
+        maskedResult);
+  }
+  //endregion
 
-
+  private Object[] maskArguments(Object[] args) {
+    Object[] maskedArgs = new Object[args.length];
+    for (int i = 0; i < args.length; i++) {
+      // Mask each argument
+      maskedArgs[i] = LoggingUtils.maskSensitiveInformation(args[i]);
+    }
+    return maskedArgs;
+  }
 }
