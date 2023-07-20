79ad3334c79bdf123089cac047b917471013bacf
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-07-20 21:45:08 +0200

    getDegreeById in DegreeController

--- api/src/main/java/com/leskiewicz/schoolsystem/degree/DegreeController.java
+++ api/src/main/java/com/leskiewicz/schoolsystem/degree/DegreeController.java
@@ -1,6 +1,7 @@
 package com.leskiewicz.schoolsystem.degree;
 
 import com.leskiewicz.schoolsystem.degree.dto.DegreeDto;
+import com.leskiewicz.schoolsystem.degree.utils.DegreeModelAssembler;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class DegreeController {
 
   private final DegreeService degreeService;
+  private final DegreeModelAssembler degreeModelAssembler;
 
   @GetMapping("/{id}")
   public ResponseEntity<DegreeDto> getDegreeById(@PathVariable Long id) {
-    
+    Degree degree = degreeService.getById(id);
+    DegreeDto degreeDto = degreeModelAssembler.toModel(degree);
+
+    return ResponseEntity.ok(degreeDto);
   }
 }
