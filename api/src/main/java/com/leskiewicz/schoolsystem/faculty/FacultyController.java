32095bfb412957c1b190dbaf461ad82d31dc918a
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-07-21 14:52:59 +0200

    Faculty patch method added

--- api/src/main/java/com/leskiewicz/schoolsystem/faculty/FacultyController.java
+++ api/src/main/java/com/leskiewicz/schoolsystem/faculty/FacultyController.java
@@ -3,15 +3,14 @@ package com.leskiewicz.schoolsystem.faculty;
 import com.leskiewicz.schoolsystem.dto.request.PageableRequest;
 import com.leskiewicz.schoolsystem.faculty.dto.CreateFacultyRequest;
 import com.leskiewicz.schoolsystem.faculty.dto.FacultyDto;
+import com.leskiewicz.schoolsystem.faculty.dto.PatchFacultyRequest;
 import com.leskiewicz.schoolsystem.faculty.utils.FacultyModelAssembler;
 import com.leskiewicz.schoolsystem.service.links.PageableLinksService;
-import com.leskiewicz.schoolsystem.utils.ValidationUtils;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,33 +18,40 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class FacultyController {
 
-  private final FacultyService facultyService;
-  private final FacultyModelAssembler facultyModelAssembler;
-  private final PageableLinksService pageableLinksService;
+    private final FacultyService facultyService;
+    private final FacultyModelAssembler facultyModelAssembler;
+    private final PageableLinksService pageableLinksService;
 
-  @GetMapping
-  public ResponseEntity<CollectionModel<FacultyDto>> getFaculties(
-      @ModelAttribute PageableRequest request) {
-    Page<Faculty> faculties = facultyService.getFaculties(request.toPageable());
-    CollectionModel<FacultyDto> facultyDtos = facultyModelAssembler.toCollectionModel(faculties);
-    pageableLinksService.addLinks(facultyDtos, faculties, FacultyController.class, request);
+    @GetMapping
+    public ResponseEntity<CollectionModel<FacultyDto>> getFaculties(@ModelAttribute PageableRequest request) {
+        Page<Faculty> faculties = facultyService.getFaculties(request.toPageable());
+        CollectionModel<FacultyDto> facultyDtos = facultyModelAssembler.toCollectionModel(faculties);
+        pageableLinksService.addLinks(facultyDtos, faculties, FacultyController.class, request);
 
-    return ResponseEntity.ok(facultyDtos);
-  }
+        return ResponseEntity.ok(facultyDtos);
+    }
 
-  @GetMapping("/{id}")
-  public ResponseEntity<FacultyDto> getFacultyById(@PathVariable Long id) {
-    Faculty faculty = facultyService.getById(id);
-    FacultyDto facultyDto = facultyModelAssembler.toModel(faculty);
+    @GetMapping("/{id}")
+    public ResponseEntity<FacultyDto> getFacultyById(@PathVariable Long id) {
+        Faculty faculty = facultyService.getById(id);
+        FacultyDto facultyDto = facultyModelAssembler.toModel(faculty);
 
-    return ResponseEntity.ok(facultyDto);
-  }
+        return ResponseEntity.ok(facultyDto);
+    }
 
-  @PostMapping
-  public ResponseEntity<FacultyDto> createFaculty(@Valid @RequestBody CreateFacultyRequest request) {
-    Faculty faculty = facultyService.createFaculty(request);
-    FacultyDto facultyDto = facultyModelAssembler.toModel(faculty);
+    @PostMapping
+    public ResponseEntity<FacultyDto> createFaculty(@Valid @RequestBody CreateFacultyRequest request) {
+        Faculty faculty = facultyService.createFaculty(request);
+        FacultyDto facultyDto = facultyModelAssembler.toModel(faculty);
 
-    return ResponseEntity.created(facultyDto.getLink("self").get().toUri()).body(facultyDto);
-  }
+        return ResponseEntity.created(facultyDto.getLink("self").get().toUri()).body(facultyDto);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<FacultyDto> updateFaculty(@RequestBody PatchFacultyRequest request, @PathVariable Long id) {
+        Faculty faculty = facultyService.updateFaculty(request, id);
+        FacultyDto facultyDto = facultyModelAssembler.toModel(faculty);
+
+        return ResponseEntity.ok(facultyDto);
+    }
 }
