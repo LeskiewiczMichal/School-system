acde3d7dc7b9c21e3f9cad40292a87aaf60770da
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-07-21 17:28:16 +0200

    PaginationLinksTest page 1, descending and page size 20

--- api/src/test/java/com/leskiewicz/schoolsystem/testUtils/CommonTests.java
+++ api/src/test/java/com/leskiewicz/schoolsystem/testUtils/CommonTests.java
@@ -14,12 +14,31 @@ public class CommonTests {
 
         // Base query
         ResultActions result = requestUtils.performGetRequest(baseApiPath, status().isOk())
-                .andExpect(jsonPath("$._links.self").value(String.format(query, 0, 10, "id", "ASC")))
-                .andExpect(jsonPath("$._links.next").value(String.format(query, 1, 10, "id", "ASC")))
-                .andExpect(jsonPath("$._links.first").value(String.format(query, 0, 10, "id", "ASC")))
-                .andExpect(jsonPath("$._links.last").value(String.format(query, lastPage, 10, "id", "ASC")));
+                .andExpect(jsonPath("$._links.self").value(String.format(query, 0, 10, "id", "asc")))
+                .andExpect(jsonPath("$._links.next").value(String.format(query, 1, 10, "id", "asc")))
+                .andExpect(jsonPath("$._links.first").value(String.format(query, 0, 10, "id", "asc")))
+                .andExpect(jsonPath("$._links.last").value(String.format(query, lastPage, 10, "id", "asc")));
 
-        
-        result = requestUtils.performGetRequest()
+        // Page 1
+        result = requestUtils.performGetRequest(baseApiPath + "?page=1", status().isOk())
+                .andExpect(jsonPath("$._links.self").value(String.format(query, 1, 10, "id", "asc")))
+                .andExpect(jsonPath("$._links.prev").value(String.format(query, 0, 10, "id", "asc")))
+                .andExpect(jsonPath("$._links.first").value(String.format(query, 0, 10, "id", "asc")))
+                .andExpect(jsonPath("$._links.last").value(String.format(query, lastPage, 10, "id", "asc")));
+
+        // Descending
+        result = requestUtils.performGetRequest(baseApiPath + "?direction=desc", status().isOk())
+                .andExpect(jsonPath("$._links.self").value(String.format(query, 0, 10, "id", "desc")))
+                .andExpect(jsonPath("$._links.next").value(String.format(query, 1, 10, "id", "desc")))
+                .andExpect(jsonPath("$._links.first").value(String.format(query, 0, 10, "id", "desc")))
+                .andExpect(jsonPath("$._links.last").value(String.format(query, lastPage, 10, "id", "desc")));
+
+        // Page size 20
+        result = requestUtils.performGetRequest(baseApiPath + "?size=20", status().isOk())
+                .andExpect(jsonPath("$._links.self").value(String.format(query, 0, 20, "id", "asc")))
+                .andExpect(jsonPath("$._links.next").value(String.format(query, 1, 20, "id", "asc")))
+                .andExpect(jsonPath("$._links.first").value(String.format(query, 0, 20, "id", "asc")))
+                .andExpect(jsonPath("$._links.last").value(String.format(query, lastPage, 20, "id", "asc")));
+
     }
 }
