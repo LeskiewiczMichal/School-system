8c70542fabe166a6dcfcd3a1282800dae6cefd28
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-07-21 22:25:55 +0200

    Create degree service function

--- api/src/main/java/com/leskiewicz/schoolsystem/degree/DegreeServiceImpl.java
+++ api/src/main/java/com/leskiewicz/schoolsystem/degree/DegreeServiceImpl.java
@@ -2,8 +2,14 @@ package com.leskiewicz.schoolsystem.degree;
 
 import com.leskiewicz.schoolsystem.degree.dto.CreateDegreeRequest;
 import com.leskiewicz.schoolsystem.error.ErrorMessages;
+import com.leskiewicz.schoolsystem.faculty.Faculty;
+import com.leskiewicz.schoolsystem.faculty.FacultyService;
+import com.leskiewicz.schoolsystem.utils.StringUtils;
+import com.leskiewicz.schoolsystem.utils.ValidationUtils;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,6 +19,8 @@ import org.springframework.stereotype.Service;
 public class DegreeServiceImpl implements DegreeService {
 
   private final DegreeRepository degreeRepository;
+  private final FacultyService facultyService;
+  private final Logger logger = LoggerFactory.getLogger(DegreeServiceImpl.class);
 
   @Override
   public Degree getById(Long id) {
@@ -39,6 +47,18 @@ public class DegreeServiceImpl implements DegreeService {
 
   @Override
   public Degree createDegree(CreateDegreeRequest request) {
+    Faculty faculty = facultyService.getByName(request.getFacultyName());
 
+    Degree degree =
+        Degree.builder()
+            .title(request.getTitle())
+            .fieldOfStudy(StringUtils.capitalizeFirstLetterOfEveryWord(request.getFieldOfStudy()))
+            .faculty(faculty)
+            .build();
+
+    ValidationUtils.validate(degree);
+    degreeRepository.save(degree);
+    logger.info("Created degree: {}", degree);
+    return degree;
   }
 }
