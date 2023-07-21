c2febd97cf85e70598541a30d2432bb9f5671183
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-07-21 23:06:48 +0200

    DegreeDtoAssertions assertDtoInColection

--- api/src/test/java/com/leskiewicz/schoolsystem/testUtils/assertions/DegreeDtoAssertions.java
+++ api/src/test/java/com/leskiewicz/schoolsystem/testUtils/assertions/DegreeDtoAssertions.java
@@ -1,3 +1,31 @@
 package com.leskiewicz.schoolsystem.testUtils.assertions;
 
-public class DegreeDtoAssertions {}
+import com.leskiewicz.schoolsystem.testModels.DegreeDto;
+import com.leskiewicz.schoolsystem.testModels.FacultyDto;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+public class DegreeDtoAssertions implements DtoAssertion<DegreeDto> {
+
+    @Override
+    public void assertDtoInCollection(ResultActions result, int index, DegreeDto dto) throws Exception {
+        result
+                .andExpect(
+                        jsonPath(String.format("$._embedded.degrees[%d].id", index)).value(dto.getId()))
+                .andExpect(
+                        jsonPath(String.format("$._embedded.degrees[%d].faculty", index)).value(dto.getFaculty()))
+                .andExpect(
+                        jsonPath(String.format("$._embedded.degrees[%d].fieldOfStudy", index)).value(dto.getFaculty()))
+                .andExpect(
+                        jsonPath(String.format("$._embedded.degrees[%d].title", index)).value(dto.getTitle()))
+                .andExpect(
+                        jsonPath(String.format("$._embedded.degrees[%d]._links.self.href", index))
+                                .value(String.format("http://localhost/api/degrees/%d", dto.getId())));
+    }
+
+    @Override
+    public void assertDto(ResultActions result, DegreeDto dto) throws Exception {
+
+    }
+}
