020fa7bd90668c84e499c6403568097e2b79f165
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-07-21 23:38:45 +0200

    GetDegreeApi status 400 on string provided for id

--- api/src/test/java/com/leskiewicz/schoolsystem/controller/DegreeControllerTest.java
+++ api/src/test/java/com/leskiewicz/schoolsystem/controller/DegreeControllerTest.java
@@ -1,26 +1,27 @@
 package com.leskiewicz.schoolsystem.controller;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.leskiewicz.schoolsystem.degree.DegreeController;
 import com.leskiewicz.schoolsystem.degree.DegreeTitle;
 import com.leskiewicz.schoolsystem.testModels.DegreeDto;
 import com.leskiewicz.schoolsystem.testUtils.CommonTests;
 import com.leskiewicz.schoolsystem.testUtils.RequestUtils;
 import com.leskiewicz.schoolsystem.testUtils.RequestUtilsImpl;
 import com.leskiewicz.schoolsystem.testUtils.assertions.DegreeDtoAssertions;
+import java.util.Arrays;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.Arguments;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.Arrays;
-import java.util.stream.Stream;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,11 +29,90 @@ import java.util.stream.Stream;
 public class DegreeControllerTest extends GenericControllerTest<DegreeDto> {
 
   private static final String BASE_URL = "/api/degrees";
+  private static final String SINGLE_URL = BASE_URL + "/";
   DegreeDtoAssertions degreeDtoAssertions;
   @Autowired private MockMvc mvc;
   // Variables
   private ObjectMapper mapper;
   private RequestUtils requestUtils;
+
+  static Stream<Arguments> getApiCollectionResponsesProvider() {
+    // Variables to check
+    DegreeDtoAssertions assertions = new DegreeDtoAssertions();
+    DegreeDto computerScience =
+        DegreeDto.builder()
+            .id(101L)
+            .title(DegreeTitle.BACHELOR_OF_SCIENCE)
+            .fieldOfStudy("Computer Science")
+            .faculty("Informatics")
+            .build();
+    DegreeDto softwareEngineering =
+        DegreeDto.builder()
+            .id(102L)
+            .title(DegreeTitle.MASTER)
+            .fieldOfStudy("Software Engineering")
+            .faculty("Informatics")
+            .build();
+    DegreeDto nano =
+        DegreeDto.builder()
+            .id(103L)
+            .title(DegreeTitle.BACHELOR)
+            .fieldOfStudy("Nano")
+            .faculty("Biology")
+            .build();
+    DegreeDto doctorNano =
+        DegreeDto.builder()
+            .id(111L)
+            .title(DegreeTitle.DOCTOR)
+            .fieldOfStudy("Nano")
+            .faculty("Mathematics")
+            .build();
+
+    DegreeDto physics =
+        DegreeDto.builder()
+            .id(119L)
+            .title(DegreeTitle.BACHELOR)
+            .fieldOfStudy("Physics")
+            .faculty("Sociology")
+            .build();
+    DegreeDto math =
+        DegreeDto.builder()
+            .id(120L)
+            .title(DegreeTitle.DOCTOR)
+            .fieldOfStudy("Math")
+            .faculty("Law")
+            .build();
+
+    // Arguments
+    Arguments noParams =
+        Arguments.of(
+            BASE_URL, Arrays.asList(computerScience, softwareEngineering, nano), assertions);
+    Arguments pageOne = Arguments.of(BASE_URL + "?page=1", Arrays.asList(doctorNano), assertions);
+    Arguments descending =
+        Arguments.of(BASE_URL + "?direction=desc", Arrays.asList(math, physics), assertions);
+    Arguments sortByIdDescending =
+        Arguments.of(
+            BASE_URL + "?sort=id&direction=desc", Arrays.asList(math, physics), assertions);
+    Arguments pageSize20 =
+        Arguments.of(
+            BASE_URL + "?size=20",
+            Arrays.asList(computerScience, softwareEngineering, nano),
+            assertions);
+
+    return Stream.of(noParams, pageOne, descending, sortByIdDescending, pageSize20);
+  }
+
+  static Stream<Arguments> getApiSingleItemErrorsProvider() {
+    Arguments status400OnStringProvided =
+        Arguments.of(
+            SINGLE_URL + "asdf",
+            status().isBadRequest(),
+            MediaType.APPLICATION_JSON.toString(),
+            "Wrong argument types provided",
+            400);
+
+    return Stream.of(status400OnStringProvided);
+  }
 
   @BeforeEach
   public void setUp() {
@@ -48,32 +128,5 @@ public class DegreeControllerTest extends GenericControllerTest<DegreeDto> {
   @Test
   public void getDegreesTestPagination() throws Exception {
     CommonTests.paginationLinksTest(requestUtils, BASE_URL, 1);
-  }
-
-  static Stream<Arguments> getApiCollectionResponsesProvider() {
-   // Variables to check
-    DegreeDtoAssertions assertions = new DegreeDtoAssertions();
-    DegreeDto computerScience = DegreeDto.builder().id(101L).title(DegreeTitle.BACHELOR_OF_SCIENCE).fieldOfStudy("Computer Science").faculty("Informatics").build();
-    DegreeDto softwareEngineering = DegreeDto.builder().id(102L).title(DegreeTitle.MASTER).fieldOfStudy("Software Engineering").faculty("Informatics").build();
-    DegreeDto nano = DegreeDto.builder().id(103L).title(DegreeTitle.BACHELOR).fieldOfStudy("Nano").faculty("Biology").build();
-    DegreeDto doctorNano = DegreeDto.builder().id(111L).title(DegreeTitle.DOCTOR).fieldOfStudy("Nano").faculty("Mathematics").build();
-
-    DegreeDto physics = DegreeDto.builder().id(119L).title(DegreeTitle.BACHELOR).fieldOfStudy("Physics").faculty("Sociology").build();
-    DegreeDto math = DegreeDto.builder().id(120L).title(DegreeTitle.DOCTOR).fieldOfStudy("Math").faculty("Law").build();
-
-    // Arguments
-    Arguments noParams = Arguments.of(
-            BASE_URL, Arrays.asList(computerScience, softwareEngineering, nano), assertions);
-    Arguments pageOne = Arguments.of(
-            BASE_URL + "?page=1", Arrays.asList(doctorNano), assertions);
-    Arguments descending = Arguments.of(
-            BASE_URL + "?direction=desc", Arrays.asList(math, physics), assertions);
-    Arguments sortByIdDescending = Arguments.of(
-            BASE_URL + "?sort=id&direction=desc", Arrays.asList(math, physics), assertions);
-    Arguments pageSize20 = Arguments.of(
-            BASE_URL + "?size=20", Arrays.asList(computerScience, softwareEngineering,nano), assertions);
-
-    return Stream.of(noParams, pageOne, descending, sortByIdDescending, pageSize20);
-
   }
 }
