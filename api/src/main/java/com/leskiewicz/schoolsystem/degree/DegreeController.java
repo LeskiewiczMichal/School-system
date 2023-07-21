d3364dafbf68216e8598c11bb3e493bc81677c85
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-07-21 22:00:41 +0200

    GetDegrees service and controller added

--- api/src/main/java/com/leskiewicz/schoolsystem/degree/DegreeController.java
+++ api/src/main/java/com/leskiewicz/schoolsystem/degree/DegreeController.java
@@ -2,12 +2,12 @@ package com.leskiewicz.schoolsystem.degree;
 
 import com.leskiewicz.schoolsystem.degree.dto.DegreeDto;
 import com.leskiewicz.schoolsystem.degree.utils.DegreeModelAssembler;
+import com.leskiewicz.schoolsystem.dto.request.PageableRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/degrees")
@@ -23,5 +23,14 @@ public class DegreeController {
     DegreeDto degreeDto = degreeModelAssembler.toModel(degree);
 
     return ResponseEntity.ok(degreeDto);
+  }
+
+  @GetMapping
+  public ResponseEntity<CollectionModel<DegreeDto>> getDegrees(
+      @ModelAttribute PageableRequest request) {
+    Page<Degree> degrees = degreeService.getDegrees(request.toPageable());
+    CollectionModel<DegreeDto> degreeDtos = degreeModelAssembler.toCollectionModel(degrees);
+
+    return ResponseEntity.ok(degreeDtos);
   }
 }
---
+++ api/src/test/java/com/leskiewicz/schoolsystem/service/DegreeServiceTest.java
@@ -0,0 +1,5 @@
+package com.leskiewicz.schoolsystem.service;
+
+public class DegreeServiceTest {
+    
+}
