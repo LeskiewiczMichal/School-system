5cb7e06387069afdebb9f9d5c640770998c9acc1
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-07-21 22:21:01 +0200

    CreateDegreeRequest

--- api/src/main/java/com/leskiewicz/schoolsystem/degree/dto/CreateDegreeRequest.java
+++ api/src/main/java/com/leskiewicz/schoolsystem/degree/dto/CreateDegreeRequest.java
@@ -1,3 +1,18 @@
 package com.leskiewicz.schoolsystem.degree.dto;
 
-public class CreateDegreeRequest {}
+import com.leskiewicz.schoolsystem.degree.DegreeTitle;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class CreateDegreeRequest {
+
+  @NotNull private final Long id;
+  @NotNull private final DegreeTitle title;
+  @NotNull private final String fieldOfStudy;
+  @NotNull private final String facultyName;
+}
