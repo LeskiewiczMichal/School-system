d6213a726fa6133c2e0870af6e68cafbc207010e
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-07-22 15:02:45 +0200

    GetDegreeByIdApi refactored

--- api/src/main/java/com/leskiewicz/schoolsystem/degree/DegreeController.java
+++ api/src/main/java/com/leskiewicz/schoolsystem/degree/DegreeController.java
@@ -2,6 +2,7 @@ package com.leskiewicz.schoolsystem.degree;
 
 import com.leskiewicz.schoolsystem.degree.dto.CreateDegreeRequest;
 import com.leskiewicz.schoolsystem.degree.dto.DegreeDto;
+import com.leskiewicz.schoolsystem.degree.utils.DegreeDtoAssembler;
 import com.leskiewicz.schoolsystem.degree.utils.DegreeModelAssembler;
 import com.leskiewicz.schoolsystem.dto.request.PageableRequest;
 import com.leskiewicz.schoolsystem.service.links.PageableLinksService;
@@ -20,13 +21,14 @@ public class DegreeController {
   private final DegreeService degreeService;
   private final DegreeModelAssembler degreeModelAssembler;
   private final PageableLinksService pageableLinksService;
+  private final DegreeDtoAssembler degreeDtoAssembler;
 
   @GetMapping("/{id}")
   public ResponseEntity<DegreeDto> getDegreeById(@PathVariable Long id) {
-    Degree degree = degreeService.getById(id);
-    DegreeDto degreeDto = degreeModelAssembler.toModel(degree);
+    DegreeDto degree = degreeService.getById(id);
+    degree = degreeDtoAssembler.toModel(degree);
 
-    return ResponseEntity.ok(degreeDto);
+    return ResponseEntity.ok(degree);
   }
 
   @GetMapping
