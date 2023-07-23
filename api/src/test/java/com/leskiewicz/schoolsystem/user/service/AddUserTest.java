cdc834f3a7a66502d26b08c5523ae814ecde88fc
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-07-23 17:20:59 +0200

    AddUser tests isolated

--- api/src/test/java/com/leskiewicz/schoolsystem/user/service/AddUserTest.java
+++ api/src/test/java/com/leskiewicz/schoolsystem/user/service/AddUserTest.java
@@ -2,16 +2,15 @@ package com.leskiewicz.schoolsystem.user.service;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 import com.leskiewicz.schoolsystem.degree.Degree;
+import com.leskiewicz.schoolsystem.error.customexception.UserAlreadyExistsException;
 import com.leskiewicz.schoolsystem.faculty.Faculty;
 import com.leskiewicz.schoolsystem.testUtils.TestHelper;
 import com.leskiewicz.schoolsystem.user.User;
 import com.leskiewicz.schoolsystem.user.UserRepository;
 import com.leskiewicz.schoolsystem.user.UserServiceImpl;
-import com.leskiewicz.schoolsystem.user.dto.UserDto;
-import com.leskiewicz.schoolsystem.user.utils.UserMapper;
-import jakarta.persistence.EntityNotFoundException;
 import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,7 +22,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class GetUserByTest {
+public class AddUserTest {
 
   // Variables
   Faculty faculty;
@@ -32,7 +31,6 @@ public class GetUserByTest {
 
   // Mocks
   @Mock private UserRepository userRepository;
-  @Mock private UserMapper userMapper;
   @InjectMocks private UserServiceImpl userService;
 
   @BeforeEach
@@ -42,42 +40,19 @@ public class GetUserByTest {
     user = TestHelper.createUser(faculty, degree);
   }
 
-  // *** GetById ***//
-
   @Test
-  public void getByIdHappyPath() {
-    UserDto dto = Mockito.mock(UserDto.class);
-    given(userRepository.findById(any(Long.class))).willReturn(Optional.of(user));
-    given(userMapper.convertToDto(user)).willReturn(dto);
-
-    UserDto testUser = userService.getById(1L);
-
-    Assertions.assertEquals(dto, testUser);
-  }
-
-  @Test
-  public void getByIdThrowsEntityNotFound() {
-    given(userRepository.findById(any(Long.class))).willReturn(Optional.empty());
-
-    Assertions.assertThrows(EntityNotFoundException.class, () -> userService.getById(1L));
-  }
-
-  // *** GetByEmail ***//
-
-  @Test
-  public void getByEmailHappyPath() {
-    given(userRepository.findByEmail(any(String.class))).willReturn(Optional.of(user));
-
-    User testUser = userService.getByEmail("email@example.com");
-
-    Assertions.assertEquals(user, testUser);
-  }
-
-  @Test
-  public void getByEmailThrowsEntityNotFound() {
+  public void addUserSavesUser() {
     given(userRepository.findByEmail(any(String.class))).willReturn(Optional.empty());
 
-    Assertions.assertThrows(
-        EntityNotFoundException.class, () -> userService.getByEmail("email@example/com"));
+    userService.addUser(user);
+
+    verify(userRepository).save(user);
+  }
+
+  @Test
+  public void throwsUserAlreadyExistsExceptionWhenUserWithGivenEmailAlreadyExists() {
+    given(userRepository.findByEmail(any(String.class))).willReturn(Optional.of(user));
+
+    Assertions.assertThrows(UserAlreadyExistsException.class, () -> userService.addUser(user));
   }
 }
