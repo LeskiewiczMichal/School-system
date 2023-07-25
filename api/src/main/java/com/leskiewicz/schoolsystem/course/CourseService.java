9cddcfea73b09f807efc0799db7a0bd4b616cc66
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-07-26 00:25:07 +0200

    GetAllCourses service method

--- api/src/main/java/com/leskiewicz/schoolsystem/course/CourseService.java
+++ api/src/main/java/com/leskiewicz/schoolsystem/course/CourseService.java
@@ -2,10 +2,14 @@ package com.leskiewicz.schoolsystem.course;
 
 import com.leskiewicz.schoolsystem.course.Course;
 import com.leskiewicz.schoolsystem.course.dto.CourseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.security.Principal;
 
 public interface CourseService {
 
-    CourseDto getById(Long id);
+  CourseDto getById(Long id);
+
+  Page<CourseDto> getCourses(Pageable pageable);
 }
--- api/src/main/java/com/leskiewicz/schoolsystem/course/CourseServiceImpl.java
+++ api/src/main/java/com/leskiewicz/schoolsystem/course/CourseServiceImpl.java
@@ -3,8 +3,12 @@ package com.leskiewicz.schoolsystem.course;
 import com.leskiewicz.schoolsystem.course.dto.CourseDto;
 import com.leskiewicz.schoolsystem.course.utils.CourseMapper;
 import com.leskiewicz.schoolsystem.error.ErrorMessages;
+import com.leskiewicz.schoolsystem.faculty.Faculty;
+import com.leskiewicz.schoolsystem.faculty.dto.FacultyDto;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,5 +26,11 @@ public class CourseServiceImpl implements CourseService {
             .orElseThrow(
                 () ->
                     new EntityNotFoundException(ErrorMessages.objectWithIdNotFound("Course", id))));
+  }
+
+  @Override
+  public Page<CourseDto> getCourses(Pageable pageable) {
+    Page<Course> courses = courseRepository.findAll(pageable);
+    return courses.map(courseMapper::convertToDto);
   }
 }
