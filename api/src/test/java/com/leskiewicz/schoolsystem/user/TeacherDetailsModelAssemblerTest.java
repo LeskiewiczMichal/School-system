a6c86030143a8a4e5ffb91cb5dc6072711e3ae09
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-07-31 19:14:11 +0200

    TEacherDetailsModelAssembler test

--- api/src/test/java/com/leskiewicz/schoolsystem/user/TeacherDetailsModelAssemblerTest.java
+++ api/src/test/java/com/leskiewicz/schoolsystem/user/TeacherDetailsModelAssemblerTest.java
@@ -1,3 +1,34 @@
 package com.leskiewicz.schoolsystem.user;
 
-public class TeacherDetailsModelAssemblerTest {}
+import com.leskiewicz.schoolsystem.faculty.Faculty;
+import com.leskiewicz.schoolsystem.testUtils.TestHelper;
+import com.leskiewicz.schoolsystem.user.teacherdetails.TeacherDetails;
+import com.leskiewicz.schoolsystem.user.teacherdetails.TeacherDetailsModelAssembler;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.hateoas.Link;
+
+@ExtendWith(MockitoExtension.class)
+public class TeacherDetailsModelAssemblerTest {
+
+  @InjectMocks private TeacherDetailsModelAssembler teacherDetailsModelAssembler;
+
+  @Test
+  public void testToModelAddsCorrectLinks() {
+    Faculty faculty = Mockito.mock(Faculty.class);
+    User user = TestHelper.createTeacher(faculty);
+    TeacherDetails teacherDetails = TestHelper.createTeacherDetails(user);
+
+    Link selfLink = Link.of("/api/users/1/teacher-details");
+    Link teacherLink = Link.of("/api/users/1");
+
+    TeacherDetails result = teacherDetailsModelAssembler.toModel(teacherDetails);
+
+    Assertions.assertEquals(selfLink.toUri(), result.getLink("self").get().toUri());
+    Assertions.assertEquals(teacherLink.toUri(), result.getLink("teacher").get().toUri());
+  }
+}
