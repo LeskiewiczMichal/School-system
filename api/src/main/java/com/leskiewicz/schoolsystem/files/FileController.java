88d15b3d46dfd23637594670332b37fb05c64ee4
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-02 21:14:42 +0200

    File downloader docs

--- api/src/main/java/com/leskiewicz/schoolsystem/files/FileController.java
+++ api/src/main/java/com/leskiewicz/schoolsystem/files/FileController.java
@@ -1,6 +1,7 @@
 package com.leskiewicz.schoolsystem.files;
 
 import jakarta.annotation.Resource;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestController
 @RequestMapping("/api/files")
@@ -18,10 +20,19 @@ public class FileController {
 
   private final FileService fileService;
 
+  /**
+   * Download file by id
+   *
+   * @param fileId id of file to download
+   * @return file
+   * @throws EntityNotFoundException if file with given id does not exist
+   * @throws MethodArgumentTypeMismatchException if fileId is not a number
+   */
   @GetMapping("/{fileId}")
   public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable Long fileId) {
     File file = fileService.getFileById(fileId);
 
+    // Set headers
     ByteArrayResource resource = new ByteArrayResource(file.getFileData());
     HttpHeaders headers = new HttpHeaders();
     headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFileName() + "\"");
