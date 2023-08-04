4546259eba4901c89221267bb244b02e035d618f
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-04 22:14:58 +0200

    CreateArticleRequest

--- api/src/main/java/com/leskiewicz/schoolsystem/article/dto/CreateArticleRequest.java
+++ api/src/main/java/com/leskiewicz/schoolsystem/article/dto/CreateArticleRequest.java
@@ -1,3 +1,30 @@
 package com.leskiewicz.schoolsystem.article.dto;
 
-public class CreateArticleRequest {}
+import com.leskiewicz.schoolsystem.article.ArticleCategory;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class CreateArticleRequest {
+
+    @NotNull(message = "Article title required")
+    private final String title;
+
+    @NotNull(message = "Article preview required")
+    private final String preview;
+
+    @NotNull(message = "Article content required")
+    private final String content;
+
+    @NotNull(message = "Article category required")
+    private final ArticleCategory category;
+
+    private final Long facultyId;
+
+    private final MultipartFile image;
+}
