89dd89a99e5d05afb5562063303587ad09b3373d
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-04 15:07:38 +0200

    Logo links to home

--- client/src/features/header/components/Header.tsx
+++ client/src/features/header/components/Header.tsx
@@ -1,24 +1,20 @@
 import { ReactComponent as Logo } from "../../../assets/logo.svg";
 import FacultiesDropdown from "./FacultiesDropdown";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="z-50 max-h-20 flex flex-auto justify-between items-center py-6 pr-8 bg-white border-b-2 border-primary">
       <div className="flex  items-center">
-        {/*<img className="h-8 w-8 mr-2" src={Logo} alt="Logo" />*/}
-        <Logo className="h-28 w-28" />
+        <Link to="/" className="h-28 w-28">
+          <Logo className="h-full w-full" />
+        </Link>
         <h1 className="text-xl md:text-2xl w-full font-bold text-primary italic">
           Aquila University
         </h1>
       </div>
 
       <div className="flex items-center  justify-end gap-4">
-        {/*<a*/}
-        {/*  className="inline-block text-sm font-bold text-primary align-baseline hover:text-primaryLighter"*/}
-        {/*  href="#"*/}
-        {/*>*/}
-        {/*  Faculties*/}
-        {/*</a>*/}
         <FacultiesDropdown />
         <a
           className="inline-block text-md font-bold text-primary align-baseline hover:text-primaryLighter"
