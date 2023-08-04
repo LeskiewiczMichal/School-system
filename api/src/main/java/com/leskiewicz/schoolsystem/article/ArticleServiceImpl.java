1327ff0bfb7b057d65cc69aff45a821cab7cf4b6
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-04 22:21:22 +0200

    Create article service method

--- api/src/main/java/com/leskiewicz/schoolsystem/article/ArticleServiceImpl.java
+++ api/src/main/java/com/leskiewicz/schoolsystem/article/ArticleServiceImpl.java
@@ -1,7 +1,12 @@
 package com.leskiewicz.schoolsystem.article;
 
 import com.leskiewicz.schoolsystem.article.dto.CreateArticleRequest;
+import com.leskiewicz.schoolsystem.authentication.utils.AuthenticationUtils;
+import com.leskiewicz.schoolsystem.authentication.utils.ValidationUtils;
 import com.leskiewicz.schoolsystem.error.ErrorMessages;
+import com.leskiewicz.schoolsystem.faculty.Faculty;
+import com.leskiewicz.schoolsystem.faculty.FacultyRepository;
+import com.leskiewicz.schoolsystem.user.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,7 +15,10 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class ArticleServiceImpl implements ArticleService {
 
+  // Repositories
   public final ArticleRepository articleRepository;
+  public final FacultyRepository facultyRepository;
+  public final UserRepository userRepository;
 
   @Override
   public Article getById(Long id) {
@@ -22,6 +30,39 @@ public class ArticleServiceImpl implements ArticleService {
 
   @Override
   public Article createArticle(CreateArticleRequest request) {
-    return null;
+    // Create a new Article object from the CreateArticleRequest
+    Article article =
+        Article.builder()
+            .title(request.getTitle())
+            .preview(request.getPreview())
+            .content(request.getContent())
+            .category(request.getCategory())
+            .build();
+
+    // Set the Faculty if available
+    if (request.getFacultyId() != null) {
+      Faculty faculty =
+          facultyRepository
+              .findById(request.getFacultyId())
+              .orElseThrow(
+                  () ->
+                      new EntityNotFoundException(
+                          ErrorMessages.objectWithIdNotFound("Faculty", request.getFacultyId())));
+      article.setFaculty(faculty);
+    }
+
+    // Get article author from authorization context
+    article.setAuthor(
+        userRepository
+            .findById(AuthenticationUtils.getAuthenticatedUser().getId())
+            .orElseThrow(
+                () ->
+                    new EntityNotFoundException(
+                        ErrorMessages.objectWithIdNotFound(
+                            "User", AuthenticationUtils.getAuthenticatedUser().getId()))));
+
+    // Save the article
+    ValidationUtils.validate(article);
+    return articleRepository.save(article);
   }
 }
