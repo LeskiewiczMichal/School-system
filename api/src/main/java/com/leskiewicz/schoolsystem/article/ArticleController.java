4f8d4145294d86e1ad2e2c06496519d98be031cc
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-04 22:32:26 +0200

    GetArticleById docs

--- api/src/main/java/com/leskiewicz/schoolsystem/article/ArticleController.java
+++ api/src/main/java/com/leskiewicz/schoolsystem/article/ArticleController.java
@@ -2,6 +2,7 @@ package com.leskiewicz.schoolsystem.article;
 
 import com.leskiewicz.schoolsystem.article.dto.CreateArticleRequest;
 import com.leskiewicz.schoolsystem.article.utils.ArticleModelAssembler;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,14 @@ public class ArticleController {
   public final ArticleService articleService;
   public final ArticleModelAssembler articleModelAssembler;
 
+  /**
+   * Get an Article by its ID
+   *
+   * @param id of the Article to get.
+   * @return the ${@link Article} with the given ID and status 200.
+   * @throws EntityNotFoundException if the Article with the given ID does not exist.
+   * @throws IllegalArgumentException if the given ID is null.
+   */
   @GetMapping
   public Article getArticleById(Long id) {
     return articleModelAssembler.toModel(articleService.getById(id));
