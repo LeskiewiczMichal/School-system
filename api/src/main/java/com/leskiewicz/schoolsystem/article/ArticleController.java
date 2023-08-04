b70ac3f21557a60f26dbf0dd18e605060f3dd86c
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-04 22:31:07 +0200

    CreateArticle API method

--- api/src/main/java/com/leskiewicz/schoolsystem/article/ArticleController.java
+++ api/src/main/java/com/leskiewicz/schoolsystem/article/ArticleController.java
@@ -1,10 +1,14 @@
 package com.leskiewicz.schoolsystem.article;
 
+import com.leskiewicz.schoolsystem.article.dto.CreateArticleRequest;
 import com.leskiewicz.schoolsystem.article.utils.ArticleModelAssembler;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @AllArgsConstructor
@@ -17,5 +21,10 @@ public class ArticleController {
   @GetMapping
   public Article getArticleById(Long id) {
     return articleModelAssembler.toModel(articleService.getById(id));
+  }
+
+  @PostMapping
+  public Article createArticle(CreateArticleRequest request) throws IOException {
+    return articleModelAssembler.toModel(articleService.createArticle(request));
   }
 }
