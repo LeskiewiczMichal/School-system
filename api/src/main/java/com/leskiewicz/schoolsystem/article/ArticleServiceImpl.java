fc68baf346ee6a748a79f8bc3d200bfa7e9ce44a
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-04 21:50:59 +0200

    Article service

--- api/src/main/java/com/leskiewicz/schoolsystem/article/ArticleServiceImpl.java
+++ api/src/main/java/com/leskiewicz/schoolsystem/article/ArticleServiceImpl.java
@@ -1,10 +1,21 @@
 package com.leskiewicz.schoolsystem.article;
 
+import com.leskiewicz.schoolsystem.error.ErrorMessages;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class ArticleServiceImpl implements ArticleService {
 
+  public final ArticleRepository articleRepository;
 
-    @Override
-    public Article getById(Long id) {
-        return null;
-    }
+  @Override
+  public Article getById(Long id) {
+    return articleRepository
+        .findById(id)
+        .orElseThrow(
+            () -> new EntityNotFoundException(ErrorMessages.objectWithIdNotFound("Article", id)));
+  }
 }
