d28ffb07f779fca27a0cb62df75094c5fa028544
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-05 15:50:16 +0200

    ArticleMapper throws exception when article has no id test

--- api/src/test/java/com/leskiewicz/schoolsystem/article/ArticleMapperTest.java
+++ api/src/test/java/com/leskiewicz/schoolsystem/article/ArticleMapperTest.java
@@ -7,6 +7,7 @@ import com.leskiewicz.schoolsystem.faculty.Faculty;
 import com.leskiewicz.schoolsystem.testUtils.TestHelper;
 import com.leskiewicz.schoolsystem.user.User;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -20,10 +21,17 @@ public class ArticleMapperTest {
 
   @InjectMocks private ArticleMapperImpl articleMapper;
 
+  User user;
+  Faculty faculty;
+
+  @BeforeEach
+  public void setup() {
+    user = Mockito.mock(User.class);
+    faculty = Mockito.mock(Faculty.class);
+  }
+
   @Test
   public void convertToDtoCorrect() {
-    User user = Mockito.mock(User.class);
-    Faculty faculty = Mockito.mock(Faculty.class);
     given(user.getFullName()).willReturn("TestUser");
     given(faculty.getName()).willReturn("TestFaculty");
     given(user.getId()).willReturn(1L);
@@ -46,5 +54,14 @@ public class ArticleMapperTest {
     ArticleDto result = articleMapper.convertToDto(article);
 
     Assertions.assertEquals(expectedArticleDto, result);
+  }
+
+  @Test
+  public void convertToDtoThrowsIllegalArgumentExceptionOnNoId() {
+    Article article = TestHelper.createArticle(user, faculty);
+    article.setId(null);
+
+    Assertions.assertThrows(
+        IllegalArgumentException.class, () -> articleMapper.convertToDto(article));
   }
 }
