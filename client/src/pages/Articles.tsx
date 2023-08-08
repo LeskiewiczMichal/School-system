209c7712e11a718d52bec32b6e2a0c3f99c2d126
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-08 23:44:13 +0200

    Fetching articles

--- client/src/pages/Articles.tsx
+++ client/src/pages/Articles.tsx
@@ -1,13 +1,18 @@
 import { Sidebar } from "../sidebar";
 import { SidebarLinkProps } from "../sidebar/components/SidebarLink";
-import { useState } from "react";
-import { ArticleCategory } from "../features/article";
+import { useEffect, useState } from "react";
+import { Article, ArticleCategory } from "../features/article";
 import { SidebarButtonProps } from "../sidebar/components/SidebarButton";
+import { useAppSelector } from "../hooks";
+import ArticleRequest from "../features/article/services/ArticleRequest";
 
 export default function Articles() {
+  const links = useAppSelector((state) => state.links);
   const [articleCategory, setArticleCategory] = useState<ArticleCategory>(
     ArticleCategory.NEWS,
   );
+  const [articles, setArticles] = useState<Article[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const sidebarButtons: SidebarButtonProps[] = [
     {
@@ -47,10 +52,32 @@ export default function Articles() {
     },
   ];
 
+  useEffect(() => {
+    const handleFetchArticles = async () => {
+      // Prepare the link
+      if (!links.articlesSearch) {
+        return;
+      }
+
+      // Call the api
+      const articles: Article[] = await ArticleRequest.getArticles({
+        link: links.articlesSearch,
+        category: articleCategory,
+        pagination: { size: 10 },
+      });
+
+      // Set the articles
+      setArticles(articles);
+      setIsLoading(false);
+    };
+
+    handleFetchArticles();
+  }, [articleCategory]);
+
   return (
     <div className={"flex h-full"}>
       <Sidebar buttons={sidebarButtons} />
-      <main className={"h-full flex flex-col px-4"}>
+      <main className={"h-full w-full flex flex-col px-4"}>
         qewr
         <p>qwer</p>
         <p>qwer</p>
