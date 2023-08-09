62e1c4f9cb519050e350cbc7846f727d36c2c293
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-10 00:46:01 +0200

    Saving progress

--- client/src/features/article/components/ArticlesSidebar.tsx
+++ client/src/features/article/components/ArticlesSidebar.tsx
@@ -8,12 +8,16 @@ export default function ArticlesSidebar(props: ArticlesSidebarProps) {
   const { category } = props;
 
   return (
-    <div className="flex flex-col z-40 flex-none gap-6 bg-grayscaleLight w-56 mt-8 xl:order-2 xl:mt-0 py-4 px-8 xl:px-0 xl:max-h-screen xl:sticky xl:top-0">
+    <div className="flex xl:flex-col z-40 flex-none gap-6 bg-grayscaleLight w-56 mt-8 xl:order-2 xl:mt-0 py-4 px-8 xl:px-0 xl:max-h-screen xl:sticky xl:top-0">
       <div className={"flex flex-col text-brandMain font-bold gap-4"}>
         <span className={""}>Aquila University</span>
         <span className={"bg-brandMain text-white w-fit px-2 py-1"}>
           {category}
         </span>
+      </div>
+      <div className={"flex flex-col text-brandMain font-bold gap-4 xl:mt-6"}>
+        <span>Share this page</span>
+        {/*    TODO: SHARE BUTTONS*/}
       </div>
     </div>
   );
