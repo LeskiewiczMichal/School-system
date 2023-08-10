033a3a195261f09238cadd09c48d0f8293a24879
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-10 10:15:42 +0200

    Sidebar share buttons

--- client/src/features/article/components/ArticlesSidebar.tsx
+++ client/src/features/article/components/ArticlesSidebar.tsx
@@ -4,6 +4,8 @@ import {
   FacebookIcon,
   TwitterShareButton,
   TwitterIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
 } from "react-share";
 
 export interface ArticlesSidebarProps {
@@ -14,17 +16,20 @@ export default function ArticlesSidebar(props: ArticlesSidebarProps) {
   const { category } = props;
 
   return (
-    <div className="flex xl:flex-col z-40 flex-none gap-6 bg-grayscaleLight w-56 mt-8 xl:order-2 xl:mt-0 py-4 px-8 xl:px-0 xl:max-h-screen xl:sticky xl:top-0">
+    <div
+      className={
+        "flex justify-between xl:justify-start xl:flex-col z-40 flex-none gap-6 border-b border-t xl:border-b-0 xl:border-t-0 bg-grayscaleLight w-full xl:w-56 mt-8 xl:order-2 xl:mt-0 py-8 xl:py-4 px-8 xl:px-0 xl:max-h-screen xl:sticky xl:top-0 mb-4 xl:mb-0"
+      }
+    >
       <div className={"flex flex-col text-brandMain font-bold gap-4"}>
         <span className={""}>Aquila University</span>
         <span className={"bg-brandMain text-white w-fit px-2 py-1"}>
           {category}
         </span>
       </div>
-      <div className={"flex flex-col text-brandMain font-bold gap-4 xl:mt-6"}>
+      <div className={"flex flex-col text-brandMain font-bold gap-2 xl:mt-6"}>
         <span>Share this page</span>
-        {/*    TODO: SHARE BUTTONS*/}
-        <div className={"flex"}>
+        <div className={"flex gap-4"}>
           <FacebookShareButton
             quote={"Check out this article from the Aquila University"}
             hashtag={"#AquilaUniversity"}
@@ -39,6 +44,14 @@ export default function ArticlesSidebar(props: ArticlesSidebarProps) {
           >
             <TwitterIcon className={"w-10  h-10 rounded"} />
           </TwitterShareButton>
+          <LinkedinShareButton
+            title={"Aquila University's page"}
+            summary={"Check out this article from the Aquila University"}
+            source={window.location.href.toString()}
+            url={window.location.href}
+          >
+            <LinkedinIcon className={"w-9  h-9 rounded"} />
+          </LinkedinShareButton>
         </div>
       </div>
     </div>
