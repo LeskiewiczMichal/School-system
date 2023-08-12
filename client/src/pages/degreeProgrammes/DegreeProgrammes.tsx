c077aa7371d33201168cf6e0a3e68f79653dc00d
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-12 15:18:53 +0200

    Save progress

--- client/src/pages/degreeProgrammes/DegreeProgrammes.tsx
+++ client/src/pages/degreeProgrammes/DegreeProgrammes.tsx
@@ -1,21 +1,31 @@
-import { Degree } from "../../features/degree";
+import { Degree, DegreeRequest } from "../../features/degree";
 import { useEffect, useState } from "react";
 import { useAppSelector } from "../../hooks";
+import { GetDegreesResponse } from "../../features/degree/services/DegreeRequest";
 
 export default function DegreeProgrammes() {
   const links = useAppSelector((state) => state.links);
   const [degrees, setDegrees] = useState<Degree[]>([]);
+  const [page, setPage] = useState<number>(0);
 
   useEffect(() => {
     const handleFetchDegrees = async () => {
       // Prepare the link
-      if (!links.degrees) {
+      if (!links.degrees.getDegrees) {
         return;
       }
 
       // Call the api
+      const response: GetDegreesResponse = await DegreeRequest.getList({
+        link: links.degrees.getDegrees,
+        pagination: { page: page },
+      });
+
+      // TODO: Set the degrees
     };
-  }, [links]);
+
+    handleFetchDegrees();
+  }, [links, page]);
 
   return (
     <div>
