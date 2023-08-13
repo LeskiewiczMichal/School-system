bb496cc004b5e0795456d7bf83be5a9fed835404
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-13 17:07:48 +0200

    Seperated degreeSearchForm into component

--- client/src/features/degree/index.ts
+++ client/src/features/degree/index.ts
@@ -1,6 +1,9 @@
 import DegreeTitle from "./types/DegreeTitle";
 import Degree from "./types/Degree";
 import DegreeRequest from "./services/DegreeRequest";
+import DegreeSearchForm, {
+  DegreeSearchFormProps,
+} from "./components/DegreeSearchForm";
 
-export { DegreeTitle, DegreeRequest };
-export type { Degree };
+export { DegreeTitle, DegreeRequest, DegreeSearchForm };
+export type { Degree, DegreeSearchFormProps };
--- client/src/pages/degreeProgrammes/DegreeProgrammes.tsx
+++ client/src/pages/degreeProgrammes/DegreeProgrammes.tsx
@@ -1,4 +1,9 @@
-import { Degree, DegreeRequest, DegreeTitle } from "../../features/degree";
+import {
+  Degree,
+  DegreeRequest,
+  DegreeSearchForm,
+  DegreeTitle,
+} from "../../features/degree";
 import { useEffect, useState } from "react";
 import { useAppSelector } from "../../hooks";
 import { GetDegreesResponse } from "../../features/degree/services/DegreeRequest";
@@ -16,7 +21,6 @@ export default function DegreeProgrammes() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   // Searching
-  const [faculties, setFaculties] = useState<ResourceNameWithLink[]>([]);
   const [page, setPage] = useState<number>(0);
   const [fieldOfStudy, setFieldOfStudy] = useState<string>("");
   const [faculty, setFaculty] = useState<string | undefined>(undefined);
@@ -30,40 +34,6 @@ export default function DegreeProgrammes() {
     totalElements: 0,
     totalPages: 0,
   });
-
-  useEffect(() => {
-    const handleFetchFaculties = async () => {
-      // Prepare the link
-      if (!links.faculties.getFaculties) {
-        return;
-      }
-
-      // Call the api
-      const response = await RequestService.performGetRequest({
-        link: links.faculties.getFaculties,
-        pagination: {
-          page: page,
-          size: 100,
-          sort: ["name", SortDirection.ASC],
-        },
-      });
-
-      // Convert the response data into resources with links
-      const facultiesArr: ResourceNameWithLink[] = [];
-      response._embedded.faculties.forEach((faculty: any) => {
-        const facultyNameWithLink: ResourceNameWithLink = {
-          name: faculty.name,
-          link: faculty._links.self.href,
-          id: faculty.id,
-        };
-        facultiesArr.push(facultyNameWithLink);
-      });
-
-      setFaculties(facultiesArr);
-    };
-
-    handleFetchFaculties();
-  }, [links]);
 
   const handleFetchDegrees = async () => {
     // Prepare the link
@@ -111,9 +81,11 @@ export default function DegreeProgrammes() {
     }
     if (event.target.name === "faculty") {
       setFaculty(event.target.value);
+      setPage(0);
     }
     if (event.target.name === "title") {
       setTitle(event.target.value as DegreeTitle);
+      setPage(0);
     }
   };
 
@@ -129,95 +101,12 @@ export default function DegreeProgrammes() {
           Aquila University offers a variety of degree programs. Find the
           perfect fit for you and embark on your educational journey with us.
         </p>
-        <form className={"border border-grayscaleDark px-4 py-8"}>
-          <h6 className={"font-bold text-brandMainNearlyBlack text-lg mb-8"}>
-            Search in our programmes
-          </h6>
-          <div className="flex mb-8">
-            <label
-              htmlFor="search-dropdown"
-              className="mb-2 text-sm font-medium text-gray-900 sr-only"
-            >
-              Field of study
-            </label>
-            {/* Search by field of study */}
-            <div className="relative w-full lg:w-2/3">
-              <input
-                type="search"
-                id="fieldOfStudy"
-                name="fieldOfStudy"
-                className="block p-4 w-full font-bold text-sm text-black bg-grayscaleLight border-2 border-grayscaleMediumDark focus:outline-none focus:ring-none focus:border-brandMain"
-                placeholder="Search for a field of study..."
-                onChange={formChangeHandler}
-                value={fieldOfStudy}
-                required
-              />
-              <button
-                type="button"
-                onClick={() => handleFetchDegrees()}
-                className="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-brandMain px-4 hover:bg-brandMainActive focus:ring-4 focus:outline-none focus:ring-blue-300"
-              >
-                <svg
-                  className="w-4 h-4"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
-                <span className="sr-only">Search</span>
-              </button>
-            </div>
-          </div>
-          <div className={"flex gap-8"}>
-            {/* Faculties dropdown */}
-            <select
-              name="faculty"
-              id="faculty"
-              className={
-                "w-full lg:w-1/5 p-2 mb-6  text-brandMain font-bold border border-grayscaleMediumDark focus:ring-bradMain focus:outline-none focus:border-brandMain"
-              }
-              onChange={formChangeHandler}
-              defaultValue={undefined}
-            >
-              <option value={undefined}>All faculties</option>
-              {faculties.map((faculty) => (
-                <option
-                  key={faculty.id.toString()}
-                  value={faculty.id.toString()}
-                >
-                  {faculty.name}
-                </option>
-              ))}
-            </select>
-            {/* Degree titles dropdown */}
-            <select
-              name="title"
-              id="title"
-              className={
-                "w-full lg:w-1/5 p-2 mb-6  text-brandMain font-bold border border-grayscaleMediumDark focus:ring-bradMain focus:outline-none focus:border-brandMain"
-              }
-              onChange={formChangeHandler}
-              defaultValue={undefined}
-            >
-              <option value={undefined}>Degree level</option>
-              <option value={DegreeTitle.BACHELOR}>Bachelor</option>
-              <option value={DegreeTitle.BACHELOR_OF_SCIENCE}>
-                Bachelor of science
-              </option>
-              <option value={DegreeTitle.MASTER}>Master</option>
-              <option value={DegreeTitle.DOCTOR}>Doctoral</option>
-              <option value={DegreeTitle.PROFESSOR}>Professor</option>
-            </select>
-          </div>
-        </form>
+        <DegreeSearchForm
+          fieldOfStudyField={fieldOfStudy}
+          formChangeHandler={formChangeHandler}
+          handleFetchDegrees={handleFetchDegrees}
+          setPage={setPage}
+        />
       </main>
     </div>
   );
