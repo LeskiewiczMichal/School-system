6c1d5471e8b1128716e9a2392d30c4d4148cbb1d
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-15 10:28:06 +0200

    addImage_throwsEntityNotFoundException_whenDegreeDoesNotExist test

--- api/src/test/java/com/leskiewicz/schoolsystem/degree/service/DegreeAddImageTest.java
+++ api/src/test/java/com/leskiewicz/schoolsystem/degree/service/DegreeAddImageTest.java
@@ -6,6 +6,7 @@ import com.leskiewicz.schoolsystem.degree.DegreeServiceImpl;
 import com.leskiewicz.schoolsystem.faculty.Faculty;
 import com.leskiewicz.schoolsystem.files.FileService;
 import com.leskiewicz.schoolsystem.testUtils.TestHelper;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -51,11 +52,26 @@ public class DegreeAddImageTest {
   public void addImage_throwsIllegalArgumentException_whenImageIsNull() {
     // Mocks
     given(degreeRepository.findById(any(Long.class)))
-            .willReturn(Optional.of(TestHelper.createDegree(Mockito.mock(Faculty.class))));
+        .willReturn(Optional.of(TestHelper.createDegree(Mockito.mock(Faculty.class))));
 
     // Call the method
-    Assertions.assertThrows(IllegalArgumentException.class, () -> {
-      degreeService.addImage(1L, null);
-    });
+    Assertions.assertThrows(
+        IllegalArgumentException.class,
+        () -> {
+          degreeService.addImage(1L, null);
+        });
+  }
+
+  @Test
+  public void addImage_throwsEntityNotFoundException_whenDegreeDoesNotExist() {
+    // Mocks
+    given(degreeRepository.findById(any(Long.class))).willReturn(Optional.empty());
+
+    // Call the method
+    Assertions.assertThrows(
+        EntityNotFoundException.class,
+        () -> {
+          degreeService.addImage(1L, null);
+        });
   }
 }
