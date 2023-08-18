d16cd6fac7136c33e7e0c53a0813b829d1458220
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-18 23:30:06 +0200

    Course page fetching course and description

--- client/src/pages/course/Course.tsx
+++ client/src/pages/course/Course.tsx
@@ -2,11 +2,14 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Course as CourseType, CourseRequest } from "../../features/course";
 import { useAppSelector } from "../../hooks";
+import RequestService from "../../utils/RequestService";
+import axios from "axios";
 
 export default function Course() {
   const { courseId } = useParams<{ courseId: string }>();
   const [course, setCourse] = useState<CourseType | null>(null);
   const courseLinks = useAppSelector((state) => state.links.courses);
+  const [description, setDescription] = useState<String | null>(null);
 
   useEffect(() => {
     const handleFetchCourse = async () => {
@@ -27,13 +30,25 @@ export default function Course() {
     handleFetchCourse();
   }, [courseId, courseLinks]);
 
+  useEffect(() => {
+    const handleFetchCourseDescription = async () => {
+      if (!course) {
+        return;
+      }
+      const resposne = await axios.get(course.description.href);
+      setDescription(resposne.data);
+    };
+
+    handleFetchCourseDescription();
+  }, [course]);
+
   if (!course) {
     return <span>Loading</span>;
   }
 
   return (
     <div>
-      <h1>Not implemented yet</h1>
+      <h1>{course.title}</h1>
     </div>
   );
 }
