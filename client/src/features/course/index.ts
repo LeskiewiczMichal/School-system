a8e19d067a1e145abe1d0ccd6bdecca65bc12b2e
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-18 17:07:41 +0200

    Added course type

---
+++ client/src/features/course/index.ts
@@ -0,0 +1,3 @@
+import Course from "./types/Course";
+
+export type { Course };
--- client/src/features/course/types/Course.ts
+++ client/src/features/course/types/Course.ts
@@ -1,3 +1,17 @@
+import APILink from "../../../type/APILink";
+
 export default interface Course {
   id: bigint;
+  title: string;
+  durationInHours: number;
+  faculty: {
+    name: string;
+    link: APILink;
+  };
+  teacher: {
+    name: string;
+    link: APILink;
+  };
+  students: APILink;
+  files: APILink;
 }
