8f9543bff1dbf0c14e374d44d722501b9adf5aad
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-20 14:11:35 +0200

    Enrollment course button

--- client/src/common_components/button/LinkButtonPrimary.tsx
+++ client/src/common_components/button/LinkButtonPrimary.tsx
@@ -4,15 +4,18 @@ import { Link } from "react-router-dom";
 export interface LinkButtonPrimaryProps {
   text: string;
   link: string;
+  fullWidthOnSmallerScreen?: boolean;
 }
 
 export default function LinkButtonPrimary(props: LinkButtonPrimaryProps) {
-  const { text, link } = props;
+  const { text, link, fullWidthOnSmallerScreen } = props;
 
   return (
     <Link
       to={link}
-      className="flex items-center justify-between gap-3 w-3/5 bg-brandMain text-center text-white font-bold text-md py-3 px-4 hover:bg-primaryDarkened"
+      className={`flex items-center justify-between gap-3 bg-brandMain text-center text-white font-bold text-md py-3 px-4 hover:bg-primaryDarkened ${
+        fullWidthOnSmallerScreen ? "w-full lg:w-3/5" : "w-3/5"
+      }`}
     >
       {text} <ArrowRight className="w-6 h-6" />
     </Link>
--- client/src/features/course/components/CourseInfoCard.tsx
+++ client/src/features/course/components/CourseInfoCard.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import DegreePageContentInterface from "../../../pages/degree/DegreePageContentInterface";
 import EnumMapper from "../../../utils/EnumMapper";
 import { useAppSelector } from "../../../hooks";
+import LinkButtonPrimary from "../../../common_components/button/LinkButtonPrimary";
 
 export interface CourseInfoCardProps {
   course: Course;
@@ -24,31 +25,50 @@ export default function CourseInfoCard(props: CourseInfoCardProps) {
         // backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="flex flex-col w-full max-w-3xl text-grayscaleDarkText h-full bg-white p-8">
-        <h2 className="text-4xl font-extrabold text-brandMain mb-6">
-          {course.title}
-        </h2>
+      <div className="flex flex-col w-full max-w-3xl justify-between text-grayscaleDarkText h-full bg-white p-8">
+        <div className={"flex flex-col gap-2 mb-4 lg:mb-0"}>
+          <h2 className="text-4xl font-extrabold text-brandMain mb-6">
+            {course.title}
+          </h2>
 
-        <span>
-          <b>Duration:</b> {course.durationInHours} hours
-        </span>
+          <span>
+            <b>Duration:</b> {course.durationInHours} hours
+          </span>
 
-        <span>
-          <b>Scope:</b>{" "}
-          {course.scope
-            .map((scope) => EnumMapper.mapScopeToString(scope))
-            .join(", ")}
-        </span>
-        {/*/!*<span><b>ECTS:</b> {course.ects}</span>*!/ // TODO: Add ECTS to the API*/}
+          <span>
+            <b>Scope:</b>{" "}
+            {course.scope
+              .map((scope) => EnumMapper.mapScopeToString(scope))
+              .join(", ")}
+          </span>
+          {/*/!*<span><b>ECTS:</b> {course.ects}</span>*!/ // TODO: Add ECTS to the API*/}
 
-        <span>
-          Teaching language{" "}
-          <b>{EnumMapper.mapLanguageToString(course.language)}</b>
-        </span>
+          <span>
+            Teaching language{" "}
+            <b>{EnumMapper.mapLanguageToString(course.language)}</b>
+          </span>
 
-        <span>The course is organized by {course.faculty.name}</span>
+          <span>The course is organized by {course.faculty.name}</span>
 
-        <span>Main leader of the course is {course.teacher.name}</span>
+          <span>Main leader of the course is {course.teacher.name}</span>
+        </div>
+
+        {isUserEnrolled ? (
+          <LinkButtonPrimary
+            link={`/courses/${course.id}/main-page`}
+            text={
+              "You are already enrolled in the course. Click here to go to the\n" +
+              " course main page"
+            }
+            fullWidthOnSmallerScreen
+          />
+        ) : (
+          <LinkButtonPrimary
+            link={`/courses/${course.id}/enroll`}
+            text={"Enroll in the course"}
+            fullWidthOnSmallerScreen
+          />
+        )}
       </div>
     </section>
   );
