578a154918c0230299b40c43cceb263d23c8b74e
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-22 00:13:29 +0200

    Added faculty mapper, updated Faculty interface

--- client/src/features/faculty/mapper/FacultyMapper.ts
+++ client/src/features/faculty/mapper/FacultyMapper.ts
@@ -1,2 +1,41 @@
-// const mapFacultyFromServer = (data: any) => {};
-export {};
+import Faculty from "../types/Faculty";
+import Mapper from "../../../type/Mapper";
+
+const mapFromServerData = (data: any): Faculty => {
+  return {
+    id: data.id,
+    name: data.name,
+    description: data.description || null,
+    articles: {
+      href: data._links.articles.href,
+      templated: data._links.articles.templated === true,
+    },
+    courses: {
+      href: data._links.courses.href,
+      templated: data._links.courses.templated === true,
+    },
+    degrees: {
+      href: data._links.degrees.href,
+      templated: data._links.degrees.templated === true,
+    },
+    students: {
+      href: data._links.students.href,
+      templated: data._links.students.templated === true,
+    },
+    teachers: {
+      href: data._links.teachers.href,
+      templated: data._links.teachers.templated === true,
+    },
+  };
+};
+
+const mapArrayFromServerData = (data: any[]): Faculty[] => {
+  return data.map((faculty) => mapFromServerData(faculty));
+};
+
+const FacultyMapper: Mapper<Faculty> = {
+  mapFromServerData,
+  mapArrayFromServerData,
+};
+
+export default FacultyMapper;
--- client/src/features/faculty/types/Faculty.ts
+++ client/src/features/faculty/types/Faculty.ts
@@ -1,7 +1,14 @@
+import APILink from "../../../type/APILink";
+
 interface Faculty {
   id: number;
-  name: string;
+  name: string | null;
   description: string;
+  articles: APILink;
+  courses: APILink;
+  degrees: APILink;
+  students: APILink;
+  teachers: APILink;
 }
 
 export default Faculty;
