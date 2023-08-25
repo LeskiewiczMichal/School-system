8e8775b7f67a0098df81c4dd4ff9616956fbafee
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-08-25 12:01:54 +0200

    MyAccount fetching courses

--- client/src/pages/MyAccount.tsx
+++ client/src/pages/MyAccount.tsx
@@ -5,45 +5,61 @@ import { BasicInformation } from "../features/user";
 import ColoredBackgroundWithPhotoOnRight from "../common_components/Card/ColoredBackgroundWithPhotoOnRight";
 import CommunityPicture from "./assets/community.webp";
 import LinkButtonBorderOnly from "../common_components/button/LinkButtonBorderOnly";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { FetchCoursesResponse } from "../features/course/services/CourseRequest";
-import { CourseRequest } from "../features/course";
+import { Course, CourseRequest } from "../features/course";
+import HaveToBeLoggedInInfo from "../common_components/HaveToBeLoggedInInfo";
+import { AppPaths } from "../App";
 
 export default function MyAccount() {
   const user = useAppSelector((state) => state.auth.data);
-  const userLinks = useAppSelector((state) => state.auth._links);
+  const links = useAppSelector((state) => state.links);
   const mobileNavBar = useAppSelector(
     (state) => state.integration.mobileNavView,
   );
 
-  if (!user) {
-    return (
-      <div className={"flex"}>
-        <h1>Not logged in</h1>
-      </div>
-    );
-  }
+  // Courses
+  const [coursePage, setCoursePage] = useState<number>(0);
+  const [courses, setCourses] = useState<Course[]>([]);
+  const [coursePaginationInfo, setCoursePaginationInfo] = useState({
+    page: 0,
+    size: 0,
+    totalElements: 0,
+    totalPages: 0,
+  });
 
   useEffect(() => {
     const handleFetchCourses = async () => {
       // Prepare the link
-      if (!degree.courses.link) {
+      if (!user || !user.courses) {
         return;
       }
-      let apiLink = degree.courses.link;
 
       // Call the api
       const response: FetchCoursesResponse = await CourseRequest.getList({
-        link: apiLink,
-        pagination: { size: 50 },
+        link: user.courses,
+        pagination: { page: coursePage },
       });
 
       setCourses(response.courses);
-      setPaginationInfo(response.paginationInfo);
+      setCoursePaginationInfo(response.paginationInfo);
     };
 
     handleFetchCourses();
-  }, [degree]);
+  }, [user, coursePage]);
+
+  // User not authenticated
+  if (!user) {
+    return (
+      <HaveToBeLoggedInInfo
+        button={{
+          text: "Go to login",
+          link: AppPaths.LOGIN,
+          color: "brandMain",
+        }}
+      />
+    );
+  }
 
   return (
     <div className={"flex h-full"}>
@@ -53,31 +69,38 @@ export default function MyAccount() {
           "pb-16 border-b sm:px-16 lg:px-0 border-grayscaleMediumDark w-full"
         }
       >
+        {/* Basic user info */}
         <section
-          className={"flex flex-col px-4 lg:px-32 py-8 mb-4 w-full lg:gap-4"}
+          className={"flex flex-col px-4 lg:px-32 py-8 mb-8 w-full lg:gap-4"}
         >
           <BasicInformation user={user} />
         </section>
-        {/*<section className={"px-6 lg:px-0"}>*/}
-        {/*    <h4 className={"my-header mt-10 mb-6"}>FACULTIES LIST:</h4>*/}
 
-        {/*    <ul*/}
-        {/*        className={*/}
-        {/*            "flex flex-col gap-8 w-full px-2 sm:px-6 lg:px-0 justify-center  md:grid md:grid-cols-2 mb-6"*/}
-        {/*        }*/}
-        {/*    >*/}
-        {/*        {faculties.map((faculty) => (*/}
-        {/*            <li key={faculty.id.toString()}>*/}
-        {/*                <LinkButtonBorderOnly*/}
-        {/*                    text={faculty.name}*/}
-        {/*                    link={`/faculties/${faculty.id}`}*/}
-        {/*                    color={"black"}*/}
-        {/*                    width={"w-full"}*/}
-        {/*                />*/}
-        {/*            </li>*/}
-        {/*        ))}*/}
-        {/*    </ul>*/}
-        {/*</section>*/}
+        {/* Courses  */}
+        <section className={"px-6 lg:px-16 border-2 border-brandMain"}>
+          <h4 className={"my-header text-brandMain mt-10 mb-6"}>
+            FACULTIES LIST:
+          </h4>
+
+          <div className={"flex flex-col lg:flex-row"}>
+            <ul
+              className={
+                "flex flex-col gap-8 w-full lg:w-1/2 justify-center mb-6"
+              }
+            >
+              {courses.map((course) => (
+                <li key={course.id.toString()}>
+                  <LinkButtonBorderOnly
+                    text={course.title}
+                    link={`/courses/${course.id}`}
+                    color={"brandMain"}
+                    width={"w-full"}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
       </main>
     </div>
   );
