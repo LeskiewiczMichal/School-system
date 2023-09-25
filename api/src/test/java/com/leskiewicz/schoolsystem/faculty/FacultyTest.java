87cdbf3420d786fee9d27491fdde770abba0d874
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-09-25 18:01:27 +0200

    Faculty hashcode test

--- api/src/test/java/com/leskiewicz/schoolsystem/faculty/FacultyTest.java
+++ api/src/test/java/com/leskiewicz/schoolsystem/faculty/FacultyTest.java
@@ -2,28 +2,66 @@ package com.leskiewicz.schoolsystem.faculty;
 
 import com.leskiewicz.schoolsystem.faculty.dto.PatchFacultyRequest;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static com.leskiewicz.schoolsystem.builders.FacultyBuilder.aFaculty;
 
 public class FacultyTest {
 
+  Faculty faculty = aFaculty().build();
+
+  @Test
+  public void updateFacultyChangesObjectProperly() {
     Faculty faculty = aFaculty().build();
+    PatchFacultyRequest changeRequest = new PatchFacultyRequest("New name");
 
+    faculty.update(changeRequest);
+
+    Assertions.assertEquals(changeRequest.name(), faculty.getName());
+  }
+
+  @Test
+  public void toStringIsFormatted() {
+    String expected = "Faculty{id=" + faculty.getId() + ", name='" + faculty.getName() + "'}";
+
+    Assertions.assertEquals(expected, faculty.toString());
+  }
+
+  @Nested
+  public class equals {
     @Test
-    public void updateFacultyChangesObjectProperly() {
-        Faculty faculty = aFaculty().build();
-        PatchFacultyRequest changeRequest = new PatchFacultyRequest("New name");
-
-        faculty.update(changeRequest);
-
-        Assertions.assertEquals(changeRequest.name(), faculty.getName());
+    public void returnsTrueForTheSameObject() {
+      Assertions.assertTrue(faculty.equals(faculty));
     }
 
     @Test
-    public void toStringIsFormatted() {
-        String expected = "Faculty{id=" + faculty.getId() + ", name='" + faculty.getName() + "'}";
-
-        Assertions.assertEquals(expected, faculty.toString());
+    public void returnsFalseForNull() {
+      Assertions.assertFalse(faculty.equals(null));
     }
+
+    @Test
+    public void returnsTrueForDifferentObjectsWithTheSameProperties() {
+      Faculty firstFaculty = aFaculty().build();
+      Faculty secondFaculty = aFaculty().build();
+
+      Assertions.assertTrue(firstFaculty.equals(secondFaculty));
+    }
+  }
+
+  @Nested
+  public class hashCode {
+    @Test
+    public void returnsTheSameValueForTheSameObject() {
+      Assertions.assertEquals(faculty.hashCode(), faculty.hashCode());
+    }
+
+    @Test
+    public void returnsDifferentValuesForDifferentObjects() {
+      Faculty firstFaculty = aFaculty().build();
+      Faculty secondFaculty = aFaculty().name("OtherName").build();
+
+      Assertions.assertNotEquals(firstFaculty.hashCode(), secondFaculty.hashCode());
+    }
+  }
 }
