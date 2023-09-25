3a85004b112fd94fa2effd559f8e89d12a60a8ea
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-09-25 18:09:21 +0200

    User equals, toString and hashCode tests

--- api/src/test/java/com/leskiewicz/schoolsystem/user/UserTest.java
+++ api/src/test/java/com/leskiewicz/schoolsystem/user/UserTest.java
@@ -2,28 +2,89 @@ package com.leskiewicz.schoolsystem.user;
 
 import com.leskiewicz.schoolsystem.user.dto.PatchUserRequest;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static com.leskiewicz.schoolsystem.builders.UserBuilder.anUser;
+
 @ExtendWith(MockitoExtension.class)
 public class UserTest {
 
   @Mock private PasswordEncoder passwordEncoder;
 
+  User user = anUser().build();
+
   @Test
   public void updateUserChangesObjectProperly() {
-    User user = User.builder().build();
+    User user = anUser().build();
     PatchUserRequest changeRequest =
-        PatchUserRequest.builder().firstName("New first name").lastName("New last name").build();
+        PatchUserRequest.builder().firstName("New first name").lastName("New last name").email("newemail@example.com").password("newPassword").build();
 
     user.update(changeRequest, passwordEncoder);
 
     Assertions.assertEquals(changeRequest.firstName(), user.getFirstName());
     Assertions.assertEquals(changeRequest.lastName(), user.getLastName());
     Assertions.assertEquals(changeRequest.email(), user.getEmail());
-    Assertions.assertEquals(changeRequest.password(), user.getPassword());
+    Assertions.assertEquals(passwordEncoder.encode(changeRequest.password()), user.getPassword());
+  }
+
+  @Test
+  public void toStringIsFormatted() {
+    User user = anUser().build();
+    String expected =
+        "User{id="
+            + user.getId()
+            + ", firstName='"
+            + user.getFirstName()
+            + "', lastName='"
+            + user.getLastName()
+            + "', email='"
+            + user.getEmail()
+            + "', role="
+            + user.getRole()
+            + "}";
+
+    Assertions.assertEquals(expected, user.toString());
+  }
+
+  @Nested
+  public class equals {
+    @Test
+    public void returnsTrueForTheSameObject() {
+      Assertions.assertTrue(user.equals(user));
+    }
+
+    @Test
+    public void returnsFalseForNull() {
+      Assertions.assertFalse(user.equals(null));
+    }
+
+    @Test
+    public void returnsTrueForDifferentObjectsWithTheSameProperties() {
+      User firstUser = User.builder().build();
+      User secondUser = User.builder().build();
+
+      Assertions.assertTrue(firstUser.equals(secondUser));
+    }
+  }
+
+  @Nested
+  public class hashCode {
+    @Test
+    public void returnsTheSameValueForTheSameObject() {
+      Assertions.assertEquals(user.hashCode(), user.hashCode());
+    }
+
+    @Test
+    public void returnsDifferentValuesForDifferentObjects() {
+      User firstUser = User.builder().build();
+      User secondUser = User.builder().firstName("differentName").build();
+
+      Assertions.assertNotEquals(firstUser.hashCode(), secondUser.hashCode());
+    }
   }
 }
