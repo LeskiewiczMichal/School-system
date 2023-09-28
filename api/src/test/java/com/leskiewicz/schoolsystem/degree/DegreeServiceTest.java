61a9cedcfdbb4daddfe0d68d045b6b17ca9ba36b
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-09-28 17:33:09 +0200

    GetById test

--- api/src/test/java/com/leskiewicz/schoolsystem/degree/DegreeServiceTest.java
+++ api/src/test/java/com/leskiewicz/schoolsystem/degree/DegreeServiceTest.java
@@ -2,8 +2,10 @@ package com.leskiewicz.schoolsystem.degree;
 
 import com.leskiewicz.schoolsystem.degree.dto.DegreeDto;
 import com.leskiewicz.schoolsystem.degree.utils.DegreeMapper;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,6 +16,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.leskiewicz.schoolsystem.builders.DegreeBuilder.createDegreeDtoListFrom;
 import static com.leskiewicz.schoolsystem.builders.DegreeBuilder.createDegreeList;
@@ -44,6 +47,26 @@ public class DegreeServiceTest {
                 degreeDtosList.get(0).getFieldOfStudy(), result.getContent().get(0).getFieldOfStudy());
         Assertions.assertEquals(
                 degreeDtosList.get(1).getFieldOfStudy(), result.getContent().get(1).getFieldOfStudy());
+    }
+
+    @Nested
+    public class getById {
+        @Test
+        public void returnsDegreeDto() {
+            when(degreeRepository.findById(any(Long.class))).thenReturn(Optional.of(degreesList.get(0)));
+            when(degreeMapper.convertToDto(any(Degree.class))).thenReturn(degreeDtosList.get(0));
+
+            DegreeDto result = degreeService.getById(1L);
+
+            Assertions.assertEquals(degreeDtosList.get(0).getFieldOfStudy(), result.getFieldOfStudy());
+        }
+
+        @Test
+        public void throwsExceptionWhenDegreeNotFound() {
+            when(degreeRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+
+            Assertions.assertThrows(EntityNotFoundException.class, () -> degreeService.getById(1L));
+        }
     }
 
 
